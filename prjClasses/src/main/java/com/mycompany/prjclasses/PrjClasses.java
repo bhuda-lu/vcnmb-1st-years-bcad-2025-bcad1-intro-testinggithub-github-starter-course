44472e8878edfc636ec4f9ba9fa54577f7124e3d
bhuda-lu <165774257+bhuda-lu@users.noreply.github.com>
2025-03-04 11:40:46 +0200

    CHANGES
    ADDED TEXT

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -11,6 +11,6 @@ package com.mycompany.prjclasses;
 public class PrjClasses {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! IM BHUDA LU");
     }
 }
