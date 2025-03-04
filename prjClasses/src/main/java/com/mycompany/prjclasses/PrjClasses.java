6a0504851910efde1fae1107ff17967ced9628de
bhuda-lu <165774257+bhuda-lu@users.noreply.github.com>
2025-03-04 11:46:00 +0200

    UPDATE

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -11,6 +11,6 @@ package com.mycompany.prjclasses;
 public class PrjClasses {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! IM BHUDA LU NICE");
+        System.out.println("Hello World! IM BHUDA LU NICE I JUST DID GITHUB");
     }
 }
